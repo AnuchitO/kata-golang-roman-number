40ad57a4397a5d03d9e4d1e605a7668aa6ee87fe
Pead Nong <anuchit.prasertsang@gmail.com>
2017-02-03 17:08:22 +0700

    refactor assertion of testcase

--- roman_test.go
+++ roman_test.go
@@ -5,23 +5,23 @@ import "testing"
 func TestInput1ShouldBeI(t *testing.T) {
 	r := roman(1)
 
-	if r != "I" {
-		t.Errorf("expected I but got '%s'", r)
-	}
+	assertEqual(t, r, "I")
 }
 
 func TestInput2ShouldBeII(t *testing.T) {
 	r := roman(2)
 
-	if r != "II" {
-		t.Errorf("expected II but got '%s'", r)
-	}
+	assertEqual(t, r, "II")
 }
 
 func TestInput3ShouldBeIII(t *testing.T) {
 	r := roman(3)
 
-	if r != "III" {
-		t.Errorf("expected III but got '%s'", r)
+	assertEqual(t, r, "III")
+}
+
+func assertEqual(t *testing.T, r, expected string) {
+	if r != expected{
+		t.Errorf("expected '%s' but got '%s'", expected, r)
 	}
 }
