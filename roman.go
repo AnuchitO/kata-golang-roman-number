c1ea7edde48a5f07d079ac9267fff1f1f3c063e2
Pead Nong <anuchit.prasertsang@gmail.com>
2017-02-03 17:11:19 +0700

    expected IV when input 4

--- roman.go
+++ roman.go
@@ -1,6 +1,9 @@
 package roman
 
 func roman(number int) string {
+	if number == 4 {
+		return "IV"
+	}
 	if number >= 1 {
 		return "I" + roman(number - 1)
 	}
--- roman_test.go
+++ roman_test.go
@@ -14,6 +14,10 @@ func TestInput3ShouldBeIII(t *testing.T) {
 	assertEqual(t, roman(3), "III")
 }
 
+func TestInput4ShouldBeIV(t *testing.T) {
+	assertEqual(t, roman(4), "IV")
+}
+
 func assertEqual(t *testing.T, r, expected string) {
 	if r != expected{
 		t.Errorf("expected '%s' but got '%s'", expected, r)
