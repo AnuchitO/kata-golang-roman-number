2d9504de1ff751c23e748cdea294d468ba7c2798
Pead Nong <anuchit.prasertsang@gmail.com>
2017-02-03 17:01:08 +0700

    expected II when input 2

--- roman.go
+++ roman.go
@@ -1,5 +1,8 @@
 package roman
 
 func roman(number int) string {
+	if number == 2 {
+		return "II"
+	}
 	return "I"
 }
--- roman_test.go
+++ roman_test.go
@@ -9,3 +9,11 @@ func TestInput1ShouldBeI(t *testing.T) {
 		t.Errorf("expected I but got '%s'", r)
 	}
 }
+
+func TestInput2ShouldBeII(t *testing.T) {
+	r := roman(2)
+
+	if r != "II" {
+		t.Errorf("expected II but got '%s'", r)
+	}
+}
