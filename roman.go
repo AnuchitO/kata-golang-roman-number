a3a5585310cb1f97e00d345922601be4c28b31ea
Pead Nong <anuchit.prasertsang@gmail.com>
2017-02-03 17:26:20 +0700

    refactor format code to same template

--- roman.go
+++ roman.go
@@ -5,13 +5,13 @@ func roman(number int) string {
 		return "X" + roman(number - 10)
 	}
 	if number >= 9 {
-		return "IX"
+		return "IX" + roman(number - 9)
 	}
 	if number >= 5 {
 		return "V" + roman(number - 5)
 	}
 	if number >= 4 {
-		return "IV"
+		return "IV" + roman(number - 4)
 	}
 	if number >= 1 {
 		return "I" + roman(number - 1)
