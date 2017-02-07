d0466731f77a39f9f00cc982a27daa666afb61b0
Pead Nong <anuchit.prasertsang@gmail.com>
2017-02-07 21:32:38 +0700

    elide Pair type

--- roman.go
+++ roman.go
@@ -2,25 +2,25 @@ package roman
 
 type Pair struct {
 	number int
-	roman string
+	roman  string
 }
 
 func roman(number int) string {
 	pairs := []Pair{
-		Pair{100, "C"},
-		Pair{90, "XC"},
-		Pair{50, "L"},
-		Pair{40, "XL"},
-		Pair{10, "X"},
-		Pair{9, "IX"},
-		Pair{5, "V"},
-		Pair{4, "IV"},
-		Pair{1, "I"},
+		{100, "C"},
+		{90, "XC"},
+		{50, "L"},
+		{40, "XL"},
+		{10, "X"},
+		{9, "IX"},
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
 	}
 
 	for _, p := range pairs {
 		if number >= p.number {
-			return p.roman + roman(number - p.number)
+			return p.roman + roman(number-p.number)
 		}
 	}
 
